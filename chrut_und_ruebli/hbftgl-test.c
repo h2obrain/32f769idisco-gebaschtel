cf4916e76b63556e012ee65b20e47128bebc0856
Oliver Meier <o.meier@camlintechnologies.com>
2019-07-19 14:41:20 +0200

    Fixed text rendering

--- chrut_und_ruebli/hbftgl-test.c
+++ chrut_und_ruebli/hbftgl-test.c
@@ -25,31 +25,46 @@
 
 #include "data/texts/lorem_ipsum_dolor.h"
 
-const char *text = "صِف خَلقَ خَودِ كَمِثلِ الشَمسِ إِذ بَزَغَت — يَحظى الضَجيعُ بِها نَجلاءَ مِعطارِ";
+const char *text_ar  = "صِف خَلقَ خَودِ كَمِثلِ الشَمسِ إِذ بَزَغَت — يَحظى الضَجيعُ بِها نَجلاءَ مِعطارِ";
 //const char *font_filename      = "fonts/amiri-regular.ttf";
-const hb_direction_t direction = HB_DIRECTION_RTL;
-const hb_script_t script       = HB_SCRIPT_ARABIC;
-const char *language           = "ar";
+const char *language_ar           = "ar";
+const hb_direction_t direction_ar = HB_DIRECTION_RTL;
+const hb_script_t script_ar       = HB_SCRIPT_ARABIC;
 
+const char *text_lat = "moin! ffi möh?";
+const char *language_lat           = "en";
+const hb_direction_t direction_lat = HB_DIRECTION_LTR;
+const hb_script_t script_lat       = HB_SCRIPT_LATIN;
 
 texture_atlas_t *atlas;
 dma2d_pixel_buffer_t atlas_surface;
 
 #define NUM_FONTS (sizeof(fonts)/sizeof(texture_font_t *))
+static texture_font_t *font_lat;
 static texture_font_t *fonts[15];
+
+//#define PRELOAD_GLYPHS
 
 dma2d_pixel_buffer_t *hbftgl_test_init() {
 	atlas = texture_atlas_new( 300, 330, 1 );
+
+//	printf("Creating latin font size: %zu\n", 16);
+	font_lat = texture_font_new_from_memory(atlas, 16, DejaVuSerif_ttf,DejaVuSerif_ttf_size, language_lat);
+#ifdef PRELOAD_GLYPHS
+	texture_font_load_glyphs(font_lat, text_lat);
+#endif
 	printf("Creating font size: %zu\n", 8);
-	fonts[0] = texture_font_new_from_memory(atlas, 8, amiri_regular_ttf,amiri_regular_ttf_size, language);
+	fonts[0] = texture_font_new_from_memory(atlas, 8, amiri_regular_ttf,amiri_regular_ttf_size, language_ar);
 //	fonts[0] = texture_font_new_from_file(atlas, 8, font_filename, language);
-	texture_font_load_glyphs(fonts[0], text );
+	texture_font_load_glyphs(fonts[0], text_ar);
 	for (size_t i=1; i<NUM_FONTS; i++) {
 		printf("Creating font size: %zu\n", 8+i);
 //		fonts[i] = texture_font_new_from_file(atlas, 12+i, font_filename, language);
 		fonts[i] = texture_font_clone(fonts[0], 8+i);
-		texture_font_load_glyphs(fonts[i], text );
-	}
+#ifdef PRELOAD_GLYPHS
+		texture_font_load_glyphs(fonts[i], text_ar );
+#endif
+		}
 
 //	dma2d_pixel_buffer_t *atlas_surface = malloc(sizeof(dma2d_pixel_buffer_t));
 	atlas_surface = (dma2d_pixel_buffer_t) {
@@ -64,87 +79,110 @@ dma2d_pixel_buffer_t *hbftgl_test_init() {
 	};
 	return &atlas_surface;
 }
+static float draw_text(
+		float y,
+		const char *text,
+		texture_font_t *font, const hb_direction_t direction, const hb_script_t script,
+		hb_buffer_t *buffer,
+		dma2d_pixel_buffer_t *render_surface
+	);
 void hbftgl_test_loop(dma2d_pixel_buffer_t *render_surface, uint32_t color) {
-	/* Create a buffer for harfbuzz to use */
-	hb_buffer_t *buffer = hb_buffer_create();
-
-	atlas_surface.in.pixel.alpha_mode.color = color;
-
 	/* display atlas */
+	atlas_surface.in.pixel.alpha_mode.color = 0xffffff;
 	dma2d_convert_copy__no_pxsrc_fix(
 			&atlas_surface, render_surface,
 			0,0, 0,0,
 			atlas_surface.width, atlas_surface.height
 		);
 
+	/* set color */
+	atlas_surface.in.pixel.alpha_mode.color = color;
+
+	/* Create a buffer for harfbuzz to use */
+	hb_buffer_t *buffer = hb_buffer_create();
+
 	/* draw text */
-	float x,y;
+	float y;
 	y = 0;
+	/* render */
+	y = draw_text(y,text_lat,font_lat,direction_lat,script_lat,buffer,render_surface);
 	for (size_t i=0; i<NUM_FONTS; ++i) {
-		hb_buffer_add_utf8( buffer, text, strlen(text), 0, strlen(text) );
-		hb_buffer_set_language( buffer, fonts[i]->language );
-//		hb_buffer_guess_segment_properties( buffer );
-		hb_buffer_set_script( buffer, script );
-		hb_buffer_set_direction( buffer, direction );
-		hb_shape( fonts[i]->hb_ft_font, buffer, NULL, 0 );
-
-		unsigned int         glyph_count;
-		hb_glyph_info_t     *glyph_info;
-		hb_glyph_position_t *glyph_pos;
-		glyph_info = hb_buffer_get_glyph_infos(buffer, &glyph_count);
-		glyph_pos  = hb_buffer_get_glyph_positions(buffer, &glyph_count);
-
-		texture_font_load_glyphs( fonts[i], text );
-
-		float width = 0.0;
-		float hres = fonts[i]->hres;
-		for (size_t j = 0; j < glyph_count; ++j) {
-			int codepoint = glyph_info[j].codepoint;
-			float x_advance = glyph_pos[j].x_advance/(float)(hres*64);
-			float x_offset = glyph_pos[j].x_offset/(float)(hres*64);
-			texture_glyph_t *glyph = texture_font_get_glyph32(fonts[i], codepoint);
-			if ( i < (glyph_count-1) )
-				width += x_advance + x_offset;
-			else
-				width += glyph->offset_x + glyph->width;
-		}
-		x = render_surface->width - width - 10 ;
-//		y = render_surface->height - i * (10+i) - 15;
-		printf("asc=%.3f desc=%.3f\n",fonts[i]->ascender,fonts[i]->descender);
-		y += fonts[i]->ascender;
-
-		for (size_t j = 0; j < glyph_count; ++j) {
-			texture_glyph_t *glyph = texture_font_get_glyph32(fonts[i], glyph_info[j].codepoint);
-			if (glyph == NULL) continue;
-
-			// because of vhinting trick we need the extra 64 (hres)
-			float x_advance = glyph_pos[j].x_advance/(float)(hres*64);
-			float x_offset = glyph_pos[j].x_offset/(float)(hres*64);
-			float y_advance = glyph_pos[j].y_advance/(float)(64);
-			float y_offset = glyph_pos[j].y_offset/(float)(64);
-
-			float x0 = x + x_offset + glyph->offset_x;
-//			float x1 = x0 + glyph->width;
-			float y0 = floor(y + y_offset + glyph->offset_y);
-//			float y1 = floor(y0 - glyph->height);
-
-			dma2d_convert_copy__no_pxsrc_fix(
-					&atlas_surface, render_surface,
-					glyph->tex_region.x, glyph->tex_region.y,
-					(int)x0,(int)y0,
-					glyph->tex_region.width, glyph->tex_region.height
-				);
-
-			x += x_advance;
-			y += y_advance;
-		}
-
-		y -= fonts[i]->descender;
-
 		/* clean up the buffer, but don't kill it just yet */
 		hb_buffer_reset(buffer);
+		/* render */
+		y = draw_text(y,text_ar,fonts[i],direction_ar,script_ar,buffer,render_surface);
 	}
 
 	/* destroy the buffer */
 	hb_buffer_destroy(buffer);
+}
+static float draw_text(
+		float y,
+		const char *text,
+		texture_font_t *font, const hb_direction_t direction, const hb_script_t script,
+		hb_buffer_t *buffer,
+		dma2d_pixel_buffer_t *render_surface
+) {
+	hb_buffer_add_utf8( buffer, text, strlen(text), 0, strlen(text) );
+	hb_buffer_set_language( buffer, font->language );
+//		hb_buffer_guess_segment_properties( buffer );
+	hb_buffer_set_script( buffer, script );
+	hb_buffer_set_direction( buffer, direction );
+	hb_shape( font->hb_ft_font, buffer, NULL, 0 );
+
+	unsigned int         glyph_count;
+	hb_glyph_info_t     *glyph_info;
+	hb_glyph_position_t *glyph_pos;
+	glyph_info = hb_buffer_get_glyph_infos(buffer, &glyph_count);
+	glyph_pos  = hb_buffer_get_glyph_positions(buffer, &glyph_count);
+
+	texture_font_load_glyphs( font, text );
+
+	float width = 0.0;
+	float hres = font->hres;
+	for (size_t i = 0; i < glyph_count; ++i) {
+		int codepoint = glyph_info[i].codepoint;
+		float x_advance = glyph_pos[i].x_advance/(float)(hres*64);
+		float x_offset = glyph_pos[i].x_offset/(float)(hres*64);
+		texture_glyph_t *glyph = texture_font_get_glyph32(font, codepoint);
+		if ( i < (glyph_count-1) )
+			width += x_advance + x_offset;
+		else
+			width += glyph->offset_x + glyph->width;
+	}
+	float x;
+	x = render_surface->width - width - 10 ;
+//		y = render_surface->height - i * (10+i) - 15;
+//	printf("asc=%.3f desc=%.3f\n",font->ascender,font->descender);
+	y += font->ascender;
+
+	for (size_t j = 0; j < glyph_count; ++j) {
+		texture_glyph_t *glyph = texture_font_get_glyph32(font, glyph_info[j].codepoint);
+		if (glyph == NULL) continue;
+
+		// because of vhinting trick we need the extra 64 (hres)
+		float x_advance = glyph_pos[j].x_advance/(float)(hres*64);
+		float x_offset = glyph_pos[j].x_offset/(float)(hres*64);
+		float y_advance = glyph_pos[j].y_advance/(float)(64);
+		float y_offset = glyph_pos[j].y_offset/(float)(64);
+
+		float x0 = x + x_offset + glyph->offset_x;
+//		float x1 = x0 + glyph->width;
+		float y0 = nearbyint(y - y_offset - glyph->offset_y);
+//			float y1 = floor(y0 - glyph->height);
+
+		dma2d_convert_copy__no_pxsrc_fix(
+				&atlas_surface, render_surface,
+				glyph->tex_region.x, glyph->tex_region.y,
+				(int)x0,(int)y0,
+				glyph->tex_region.width, glyph->tex_region.height
+			);
+
+		x += x_advance;
+		y += y_advance;
+	}
+
+	y -= font->descender;
+
+	return y;
 }
